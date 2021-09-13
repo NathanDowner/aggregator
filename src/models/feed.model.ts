b9d45df00d3727dbabffe0c87d984d194fe39ae0
Nathan Downer <nathandowner123@gmail.com>
2021-09-12 19:15:56 -0500

    create feed related models

--- src/models/feed.model.ts
+++ src/models/feed.model.ts
@@ -7,3 +7,38 @@ export interface Source {
   name: string;
   link: string;
 }
+
+export interface Article {
+  categories: string[];
+  content: string;
+  'content:encoded': string;
+  contentSnippet: string;
+  'content:encodedSnippet': string;
+  creator: string;
+  guid: string;
+  isoDate: string;
+  link: string;
+  pubDate: string;
+  title: string;
+}
+
+export interface FeedImage {
+  height: string;
+  link: string;
+  title: string;
+  url: string;
+  width: string;
+}
+
+export interface RSSBase {
+  description: string;
+  fedUrl: string;
+  generator: string;
+  image: FeedImage;
+  items: Article[];
+  language: string;
+  lastBuildDate: string;
+  link: string;
+  paginationLinks: { self: string };
+  title: string;
+}
