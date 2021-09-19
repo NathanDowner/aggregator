0e84d34d1e99e8307a377cb4f3206659143488a2
Nathan Downer <nathandowner123@gmail.com>
2021-09-19 13:21:11 -0500

    add feed data to component state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,41 @@ import MainDisplay from '../components/MainDisplay';
 import SideBar from '../components/SideBar';
 import { Feed } from '../models/feed.model';
 
-const feeds: Feed[] = [
-  {
-    name: 'Tech News',
-    sources: [
-      { name: 'CNET', link: 'https://www.cnet.com/rss/all/' },
-      { name: 'FPT', link: 'https://www.frontpagetech.com/feed/' },
-      // { name: '9to5 Mac', link: 'https://9to5mac.com/feed/' },
-    ],
-  },
-  // { name: 'feed 2', sources: [{ name: 'CNET', link: '' }] },
-  // { name: 'feed 3', sources: [{ name: 'CNET', link: '' }] },
-];
+// const feeds: Feed[] = [
+//   {
+//     name: 'Tech News',
+//     sources: [
+//       { name: 'CNET', link: 'https://feed.cnet.com/feed/all' },
+//       { name: 'FPT', link: 'https://www.frontpagetech.com/feed/' },
+//       { name: '9to5 Mac', link: 'https://9to5mac.com/feed/' },
+//     ],
+//   },
+//   {
+//     name: 'World News',
+//     sources: [
+//       { name: 'CNN', link: 'http://rss.cnn.com/rss/edition_world.rss' },
+//     ],
+//   }
+// ];
 
 export default function Home() {
   const [activeFeedIndex, setActiveFeedIndex] = useState(0);
+  const [feeds, setFeeds] = useState<Feed[]>([
+    {
+      name: 'Tech News',
+      sources: [
+        { name: 'CNET', link: 'https://feed.cnet.com/feed/all' },
+        { name: 'FPT', link: 'https://www.frontpagetech.com/feed/' },
+        { name: '9to5 Mac', link: 'https://9to5mac.com/feed/' },
+      ],
+    },
+    {
+      name: 'World News',
+      sources: [
+        { name: 'CNN', link: 'http://rss.cnn.com/rss/edition_world.rss' },
+      ],
+    },
+  ]);
   return (
     <div className="flex min-h-full">
       <Head>
