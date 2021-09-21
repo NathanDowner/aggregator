424b992bae9f50b3e5b95ed7bc2148877e28e1bc
Nathan Downer <nathandowner123@gmail.com>
2021-09-20 23:38:44 -0500

    implement sorting

--- src/components/SortControls.tsx
+++ src/components/SortControls.tsx
@@ -1,11 +1,21 @@
 import { SortDescendingIcon, SortAscendingIcon } from '@heroicons/react/solid';
+import { SortConfig } from '../models/sort.model';
 
-const SortControls = () => {
+type SortControlsProps = {
+  sortConfig: SortConfig;
+  onSort: (key: string) => void;
+};
+
+const SortControls: React.FC<SortControlsProps> = ({ sortConfig, onSort }) => {
+  const SortIcon =
+    sortConfig.direction === 'ascending'
+      ? SortAscendingIcon
+      : SortDescendingIcon;
   return (
     <div className="flex items-center space-x-1 cursor-pointer">
-      <div className="bg-white p-1 pl-3 rounded-l-full ">Date</div>
-      <div className="bg-white p-1 pr-3 rounded-r-full ">
-        <SortDescendingIcon className="h-5" />
+      <div className="bg-white p-1 pl-2 rounded-l-full ">Date</div>
+      <div className="bg-white p-1 pr-2 rounded-r-full ">
+        <SortIcon onClick={() => onSort('isoDate')} className="h-5" />
       </div>
     </div>
   );
