0c97d1e2d6266c337abd667f5951b805ab11e276
Nathan Downer <nathandowner123@gmail.com>
2021-09-21 23:52:42 -0500

    create SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import { SearchIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+import useDebounce from '../hooks/useDebounce';
+
+type SearchBarProps = {
+  onSearch: (searchTerm: string) => void;
+};
+
+const DEBOUNCE_DURATION = 500;
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  useDebounce(() => onSearch(searchTerm), DEBOUNCE_DURATION, [searchTerm]);
+
+  const handleChange = (e) => setSearchTerm(e.target.value);
+  return (
+    <div className="flex items-center bg-white input shadow-sm focus-within:ring-2 focus-within:ring-blue-400">
+      <SearchIcon className="h-4 mr-1 text-gray-400 flex-shrink-0" />
+      <input
+        type="text"
+        className="text-md text-gray-400 flex-1 focus:outline-none"
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="Search"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
