54ab638d5c622a396be833dced4252f37c40e68f
Nathan Downer <nathandowner123@gmail.com>
2021-10-02 01:40:07 -0500

    add masonry layout

--- src/components/NewsFeed.tsx
+++ src/components/NewsFeed.tsx
@@ -1,3 +1,4 @@
+import Masonry from 'react-masonry-css';
 import { FeedArticle } from '../models/feed.model';
 import Article from './Article';
 
@@ -6,12 +7,23 @@ type NewsFeedProps = {
 };
 
 const NewsFeed: React.FC<NewsFeedProps> = ({ articles }) => {
+  const breakpointColumnsObj = {
+    950: 1,
+    1360: 2,
+    default: 3,
+  };
+
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    // <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="masonry-grid"
+      columnClassName="masonry-grid-column"
+    >
       {articles.map((article, idx) => (
         <Article key={idx} article={article} />
       ))}
-    </div>
+    </Masonry>
   );
 };
 
