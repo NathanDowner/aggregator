4d8590790617ea243e5f0d4c57833f0959d9f7d8
Nathan Downer <nathandowner123@gmail.com>
2021-10-02 21:08:44 -0500

    add rotation animation to sort controls

--- src/components/SortControls.tsx
+++ src/components/SortControls.tsx
@@ -1,4 +1,4 @@
-import { SortDescendingIcon, SortAscendingIcon } from '@heroicons/react/solid';
+import { SortDescendingIcon } from '@heroicons/react/solid';
 import { SortConfig } from '../models/sort.model';
 
 type SortControlsProps = {
@@ -7,15 +7,16 @@ type SortControlsProps = {
 };
 
 const SortControls: React.FC<SortControlsProps> = ({ sortConfig, onSort }) => {
-  const SortIcon =
-    sortConfig.direction === 'ascending'
-      ? SortAscendingIcon
-      : SortDescendingIcon;
   return (
     <div className="flex items-center space-x-1 cursor-pointer">
       <div className="bg-white p-1 pl-2 rounded-l-full ">Date</div>
       <div className="bg-white p-1 pr-2 rounded-r-full ">
-        <SortIcon onClick={() => onSort('isoDate')} className="h-5" />
+        <SortDescendingIcon
+          onClick={() => onSort('isoDate')}
+          className={`h-5 transition-transform ${
+            sortConfig.direction === 'ascending' && 'rotate-180'
+          }`}
+        />
       </div>
     </div>
   );
