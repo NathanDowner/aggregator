21d7966ffd621a220f74f67ab4f0786d1c3ebb68
Nathan Downer <nathandowner123@gmail.com>
2021-10-20 22:15:35 -0500

    add fade in transition to animations export

---
+++ src/components/animations/FadeIn.tsx
@@ -0,0 +1,23 @@
+import { Transition } from '@headlessui/react';
+
+type Props = {
+  reveal: boolean;
+};
+
+const FadeIn: React.FC<Props> = ({ children, reveal }) => {
+  return (
+    <Transition
+      show={reveal}
+      enter="transition-opacity"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export default FadeIn;
