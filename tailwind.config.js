2ee849df9fe815dc9babba51085c9badd7ebd3c4
Nathan Downer <nathandowner123@gmail.com>
2021-11-06 00:35:48 -0500

    add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,25 @@ module.exports = {
       colors: {
         primary: colors.blue,
       },
+      rotate: {
+        '-full': '-360deg',
+      },
+      animation: {
+        'animate-spin-counterclockwise':
+          'counterClockwise 500ms linear infinite',
+      },
+      keyframes: {
+        counterClockwise: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      rotate: ['hover'],
+    },
   },
   plugins: [],
 };
